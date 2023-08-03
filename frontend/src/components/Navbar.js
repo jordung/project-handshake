53dbf4b1512d3786e8958f46e0d61dd0e2888af6
jordung <jordanayd@gmail.com>
2023-08-03 13:34:17 +0800

    added in error page and auth0

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,92 +1,108 @@
 import logo from "../assets/logo/large-logo-black.png";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
 
 function Navbar() {
-  const location = useLocation();
   const navigate = useNavigate();
 
-  if (location.pathname === "/signup" || location.pathname === "/login") {
-    return null;
-  }
+  const { loginWithRedirect, isAuthenticated, logout, user } = useAuth0();
 
   return (
-    <div className="navbar fixed bg-white">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+    <div>
+      <div className="navbar fixed bg-white">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </label>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+              <li>
+                <p
+                  className="cursor-pointer"
+                  onClick={() => navigate("/login")}
+                >
+                  Log in
+                </p>
+              </li>
+              <li>
+                <a>Seek</a>
+              </li>
+              <li>
+                <a>Contribute</a>
+              </li>
+              <li>
+                <a>About</a>
+              </li>
+            </ul>
+          </div>
+          <div className="hidden lg:flex">
+            <ul className="menu menu-horizontal px-1 font-medium">
+              <li>
+                <a>Seek</a>
+              </li>
+              <li>
+                <a>Contribute</a>
+              </li>
+              <li>
+                <a>About</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="navbar-center">
+          <button className="btn btn-link">
+            <img
+              className="h-7 md:h-10 object-contain"
+              src={logo}
+              alt="logo"
+              onClick={() => navigate("/")}
+            />
+          </button>
+        </div>
+        <div className="navbar-end gap-4">
+          {/* ! TO REMOVE LOGOUT */}
+          <button
+            className="btn btn-ghost btn-sm normal-case hidden lg:inline-block lg:btn-md"
+            onClick={() =>
+              logout({ logoutParams: { returnTo: window.location.origin } })
+            }
           >
-            <li>
-              <p className="cursor-pointer" onClick={() => navigate("/login")}>
-                Log in
-              </p>
-            </li>
-            <li>
-              <a>Seek</a>
-            </li>
-            <li>
-              <a>Contribute</a>
-            </li>
-            <li>
-              <a>About</a>
-            </li>
-          </ul>
-        </div>
-        <div className="hidden lg:flex">
-          <ul className="menu menu-horizontal px-1 font-medium">
-            <li>
-              <a>Seek</a>
-            </li>
-            <li>
-              <a>Contribute</a>
-            </li>
-            <li>
-              <a>About</a>
-            </li>
-          </ul>
+            Log out
+          </button>
+          <button
+            className="btn btn-ghost btn-sm normal-case hidden lg:inline-block lg:btn-md"
+            onClick={() => loginWithRedirect()}
+          >
+            Log in
+          </button>
+
+          <button
+            className="btn btn-neutral btn-sm normal-case md:btn-md"
+            onClick={() => navigate("/signup")}
+          >
+            Sign up
+          </button>
         </div>
       </div>
-      <div className="navbar-center">
-        <button className="btn btn-link">
-          <img
-            className="h-7 md:h-10 object-contain"
-            src={logo}
-            alt="logo"
-            onClick={() => navigate("/")}
-          />
-        </button>
-      </div>
-      <div className="navbar-end gap-4">
-        <button
-          className="btn btn-ghost btn-sm normal-case hidden lg:inline-block lg:btn-md"
-          onClick={() => navigate("/login")}
-        >
-          Log in
-        </button>
-        <button
-          className="btn btn-neutral btn-sm normal-case md:btn-md"
-          onClick={() => navigate("/signup")}
-        >
-          Sign up
-        </button>
-      </div>
+      <Outlet />
     </div>
   );
 }
---
+++ frontend/src/pages/Error.js
@@ -0,0 +1,37 @@
+import { useNavigate } from "react-router-dom";
+import error from "../assets/error/error.svg";
+
+function Error() {
+  const navigate = useNavigate();
+  // TODO: Fix responsive screen size
+  return (
+    <div className="flex flex-col px-8 pt-8 pb-0 text-center min-h-screen justify-start items-center prose md:min-w-full md:overflow-hidden md:justify-center md:pt-72">
+      <div className="fixed bottom-0 -z-10 md:top-0">
+        <img
+          className="h-96 object-cover mb-0 md:h-[120vh]"
+          src={error}
+          alt="error"
+        />
+      </div>
+      <h2 className="mt-28">This page is cordoned off!</h2>
+      <p className="mb-2 md:w-2/3 lg:w-1/3">
+        Oops! It seems like the path to volunteering has encountered a hiccup.
+        Just like a stray pebble on a smooth trail, our app has stumbled upon a
+        digital pothole, swallowing some well-intentioned volunteers. But fear
+        not, we're already setting up markers to mend the virtual road.
+      </p>
+      <p className="my-2 md:w-2/3 lg:w-1/3">
+        In the meantime, why not explore other areas of the app or learn more
+        about the incredible impact our volunteers have made so far?
+      </p>
+      <button
+        className="btn btn-neutral mt-16 md:mt-1"
+        onClick={() => navigate("/")}
+      >
+        Back
+      </button>
+    </div>
+  );
+}
+
+export default Error;
